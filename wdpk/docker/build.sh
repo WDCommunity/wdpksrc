93e89e11c68c6f14794b27f209aa1a545a2228f2
Stefaan Ghysels <stefaang@gmail.com>
2018-04-24 12:43:36 +0200

    Updated build file

--- wdpk/docker/build.sh
+++ wdpk/docker/build.sh
@@ -1,22 +1,26 @@
 #!/bin/sh
 
-APP_DIR=docker
-CWD=`pwd`
-VERSION=`awk '/Version/{print $NF}' apkg.rc`
-echo "Building version $VERSION"
+APP_NAME="$(basename $(pwd))"
+DATE="$(date +"%m%d%Y")"
+CWD="$(pwd)"
+VERSION="$(awk '/Version/{print $NF}' apkg.rc)"
 
-mksapkg -E -s -m MyCloudPR2100
-mksapkg -E -s -m MyCloudPR4100
+echo "Building ${APP_NAME} version ${VERSION}"
 
-echo "Move binaries outside of vagrant"
-cd ..
-mv MyCloud* /vagrant/bins
-mv WDMy* /vagrant/bins
+MODELS="WDMyCloudEX4 WDMyCloudEX2 WDMyCloudEX4100 WDMyCloudDL4100 WDMyCloudEX2100 WDMyCloudDL2100 MyCloudEX2Ultra MyCloudPR4100 MyCloudPR2100"
+
+for model in $MODELS; do
+  ../../mksapkg -E -s -m $model > /dev/null
+done
+
+echo "Move binaries"
+
+mkdir -p ../../packages
+find .. -maxdepth 1 -name "*.bin*" -exec rename 's#\('$DATE'\)##' {} \;
+mv ../*.bin ../../packages
 
 echo "Bundle sources"
-SRC_TAR="${APP_DIR}_src_${VERSION}.tar.gz"
-tar czf $SRC_TAR $APP_DIR
+SRC_TAR="../../packages/${APP_NAME}_src_${VERSION}.tar.gz"
+tar -czf $SRC_TAR .
 
-echo "Move source tar outside of vagrant"
-mv $SRC_TAR /vagrant/bins
-cd $APP_DIR
+
