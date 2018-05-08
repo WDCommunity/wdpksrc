be1de9df17c946b4a8196442c78fc880f0585ace
Stefaan Ghysels <stefaan.ghysels@wdc.com>
2018-05-08 13:43:31 +0200

    Stop all PIDs and fetch recent certificates

--- wdpk/duplicati/init.sh
+++ wdpk/duplicati/init.sh
@@ -6,6 +6,12 @@ path=$1
 
 echo "INIT linking files from path: $path" >> /tmp/debug_apkg
 
+# setup links to mono
+ln -sf /shares/Volume_1/Nas_Prog/mono /usr/local/mono
+
+# ensure the certificates are up to date
+/usr/local/mono/bin/cert-sync /etc/ssl/certs/ca-certificates.crt
+
 # create folder for the webpage
 WEBPATH="/var/www/duplicati/"
 mkdir -p $WEBPATH
