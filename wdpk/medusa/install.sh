08b44d9c988fadae2bf6c359a69a3d4ce34a90a9
Stefaan Ghysels <stefaang@gmail.com>
2018-05-14 22:32:53 +0200

    Recover config on upgrade

--- wdpk/medusa/install.sh
+++ wdpk/medusa/install.sh
@@ -5,20 +5,21 @@ PATH="/opt/bin:/opt/sbin:$PATH"
 [ -f /tmp/debug_apkg ] && echo "APKG_DEBUG: $0 $@" >> /tmp/debug_apkg
 
 path_src=$1
-path_dst=$2
+NAS_PROG=$2
 
 log=/tmp/debug_apkg
 
 APKG_MODULE="medusa"
-APKG_PATH="${path_dst}/${APKG_MODULE}"
+APKG_PATH="${NAS_PROG}/${APKG_MODULE}"
 APKG_CONFIG="${APKG_PATH}/settings.conf"
-APKG_BACKUP_CONFIG="/mnt/HD/HD_a2/.systemfile/medusa.conf"
+APKG_BACKUP="${NAS_PROG}/${APKG_MODULE}_backup"
+APKG_BACKUP_CONFIG="${APKG_BACKUP}/settings.conf"
 
 # create backup of medusa configuration
 cp -f ${APKG_CONFIG} ${APKG_BACKUP_CONFIG}
 
 # install all package scripts to the proper location
-cp -rf $path_src $path_dst 
+cp -rf $path_src $NAS_PROG
 
 echo "install requirements" >> $log
 opkg install busybox git git-http python python-pyopenssl >> $log 2>&1
@@ -33,11 +34,10 @@ echo "update the bootscript to the local git repo" >> $log
 sed -i "s|^PKG_DIR=.*|PKG_DIR=$APKG_PATH|" $APKG_PATH/bootscript 
 
 # restore previous config
-if [ -f $APKG_BACKUP_CONFIG ]
-then
+if [ -f $APKG_BACKUP_CONFIG ] ; then
    echo "Addon Medusa (install.sh) restore configs" >> $log
    cp ${APKG_BACKUP_CONFIG} ${APKG_CONFIG}
-   rm -f ${APKG_BACKUP_CONFIG}
+   rm -rf ${APKG_BACKUP}
 fi
 echo "Addon Medusa (install.sh) done" >> $log
 
--- wdpk/medusa/preinst.sh
+++ wdpk/medusa/preinst.sh
@@ -2,15 +2,16 @@
 
 [ -f /tmp/debug_apkg ] && echo "APKG_DEBUG: $0 $@" >> /tmp/debug_apkg
 
-APKG_PATH=$1
+NAS_PROG="/shares/Volume_1/Nas_Prog"
 
-APKG_MODULE="NZBget"
-APKG_BACKUP_PATH=${APKG_PATH}/../${APKG_MODULE}_backup
-
+APKG_MODULE="medusa"
+APKG_PATH="${NAS_PROG}/${APKG_MODULE}"
+APKG_BACKUP_PATH="${NAS_PROG}/${APKG_MODULE}_backup"
+APKG_CONFIG="${APKG_PATH}/settings.conf"
 
 # backup config files and user settings
-if [ ! -d ${APKG_BACKUP_PATH}] ; then
+if [ -f $APKG_CONFIG ] ; then
     # copy config to /tmp
     mkdir -p ${APKG_BACKUP_PATH}
-    mv -f $APKG_PATH/config ${APKG_BACKUP_PATH}
+    mv -f ${APKG_CONFIG} ${APKG_BACKUP_PATH}
 fi
