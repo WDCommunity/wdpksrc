1ac928f9ab3b5e96f2622617c11c358fb5aec449
Stefaan Ghysels <stefaang@gmail.com>
2018-05-14 21:54:16 +0200

    Fixed bash complaint

--- wdpk/medusa/start.sh
+++ wdpk/medusa/start.sh
@@ -3,6 +3,6 @@
 PATH="/opt/bin:/opt/sbin:$PATH"
 SCRIPT=/opt/etc/init.d/S91Medusa
 
-[ ! -f $SCRIPT ] echo "Medusa startup script not found" >> /tmp/debug_apkg
+[ ! -f $SCRIPT ] && echo "Medusa startup script not found" >> /tmp/debug_apkg
 
 $SCRIPT start
