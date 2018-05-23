7b37ac1f8b65302fe834e6027ab94a952fb50c99
Stefaan Ghysels <stefaan.ghysels@wdc.com>
2018-05-23 13:05:39 +0200

    Updated build scripts

--- wdpk/docker/build.sh
+++ wdpk/docker/build.sh
@@ -7,7 +7,7 @@ VERSION="$(awk '/Version/{print $NF}' apkg.rc)"
 
 echo "Building ${APP_NAME} version ${VERSION}"
 
-MODELS="WDMyCloudEX4 WDMyCloudEX2 WDMyCloudEX4100 WDMyCloudDL4100 WDMyCloudEX2100 WDMyCloudDL2100 MyCloudEX2Ultra MyCloudPR4100 MyCloudPR2100"
+MODELS="WDMyCloudEX4 WDMyCloudEX2 WDMyCloudMirror WDMyCloud WDMyCloudEX4100 WDMyCloudDL4100 WDMyCloudEX2100 WDMyCloudDL2100 WDMyCloudMirrorGen2 MyCloudEX2Ultra MyCloudPR4100 MyCloudPR2100"
 
 for model in $MODELS; do
   ../../mksapkg -E -s -m $model > /dev/null
@@ -15,12 +15,13 @@ done
 
 echo "Move binaries"
 
-mkdir -p ../../packages
+RELEASE_DIR="../../packages/${APP_NAME}"
+mkdir -p "${RELEASE_DIR}"
 find .. -maxdepth 1 -name "*.bin*" -exec rename 's#\('$DATE'\)##' {} \;
-mv ../*.bin ../../packages
+mv ../*.bin "${RELEASE_DIR}"
 
 echo "Bundle sources"
-SRC_TAR="../../packages/${APP_NAME}_src_${VERSION}.tar.gz"
+SRC_TAR="${RELEASE_DIR}/${APP_NAME}_src_${VERSION}.tar.gz"
 tar -czf $SRC_TAR .
 
 
