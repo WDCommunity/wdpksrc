2135c7b125698d8f646759389ccc8119b3ef3deb
Stefaan Ghysels <stefaang@gmail.com>
2018-05-24 00:52:05 +0200

    Setup permanent appdata dir

--- wdpk/emby/start.sh
+++ wdpk/emby/start.sh
@@ -2,7 +2,10 @@
 
 echo "APKG_DEBUG: starting emby" >> /tmp/debug_apkg
 
+APP_DIR=$1
+
+export EMBY_DATA=${APP_DIR}/appdata
 PID_FILE=/var/run/emby-server.pid
 
-APP_DIR=$1
+
 $APP_DIR/bin/emby-server -p $PID_FILE -m
