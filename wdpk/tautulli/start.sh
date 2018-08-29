61945e4957563d06805a7219c875a590ae9e6320
Stefaan Ghysels <stefaang@gmail.com>
2018-08-29 17:56:06 +0200

    Keep config on upgrade and some minor fixes

--- wdpk/tautulli/start.sh
+++ wdpk/tautulli/start.sh
@@ -5,7 +5,4 @@ export PATH=/opt/bin:/opt/sbin:$PATH
 
 echo "APKG_DEBUG: starting PlexPy on port 8282" >> /tmp/debug_apkg
 
-APPDIR=$1
-
-cd "${APPDIR}/Tautulli"
-python ./PlexPy.py --port 8282 --daemon --pidfile /var/run/plexpy.pid
+/opt/etc/init.d/S43Tautulli.sh start
