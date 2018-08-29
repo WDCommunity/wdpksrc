ff31ee1ac608dc99df4be7888b215f887d98ae01
Stefaan Ghysels <stefaan.ghysels@wdc.com>
2018-08-29 15:17:15 +0200

    Fixed installation issues and made git the default source

--- wdpk/tautulli/install.sh
+++ wdpk/tautulli/install.sh
@@ -2,46 +2,37 @@
 
 [ -f /tmp/debug_apkg ] && echo "APKG_DEBUG: $0 $@" >> /tmp/debug_apkg
 
-path_src=$1
-path_dst=$2
+INSTALL_DIR=$1
+NAS_PROG=$2
 
 log=/tmp/debug_apkg
 
 APKG_MODULE="tautulli"
 APKG_PATH="${path_dst}/${APKG_MODULE}"
-APKG_CONFIG="${APKG_PATH}/${APKG_MODULE}.conf"
-APKG_BACKUP_CONFIG="/mnt/HD/HD_a2/.systemfile/{APKG_MODULE}.conf"
-
-# create backup of nzbget configuration
-cp -f ${APKG_CONFIG} ${APKG_BACKUP_CONFIG}
+APKG_CONFIG="${APKG_PATH}/config.ini"
+APKG_CONFIG_BACKUP="/mnt/HD/HD_a2/.systemfile/{APKG_MODULE}.ini"
+DATA_DIR="${APKG_PATH}/data"
+DATA_DIR_BACKUP="/mnt/HD/HD_a2/.systemfile/{APKG_MODULE}_data"
 
 # install all package scripts to the proper location
-cp -rf $path_src $path_dst
+cp -rf ${INSTALL_DIR} ${NAS_PROG}
 
 # setup secure downloads
 curl --remote-name --time-cond cacert.pem https://curl.haxx.se/ca/cacert.pem
 mv cacert.pem /etc/ssl/cert.pem
 
-# download the latest PlexPy package from github
-wget https://github.com/Tautulli/Tautulli/archive/master.zip -P "${APKG_PATH}" 2>&1 >> $log
-
-result=$?
-echo "Download ${APKG_MODULE} RC: $result" >> $log
-
-# unpack plexpy-master
-unzip "${APKG_PATH}/master.zip" -d "${APKG_PATH}" 2>&1 >> $log
-
-result=$?
-echo "Unpack RC: $result" >> $log
-
-# remove the archive
-rm "${APKG_PATH}/master.zip"
-
 # install pip to get pkg_resources module
-wget https://bootstrap.pypa.io/get-pip.py
-/opt/bin/opkg install python-light
-/opt/bin/python get-pip.py
-rm get-pip.py
+/opt/bin/opkg install python-light python-pip git git-http
 
-echo "Addon Tautulli (install.sh) done" >> $log
+# get the Tautulli source code
+/opt/bin/git clone https://github.com/Tautulli/Tautulli.git
+
+# restore config
+if [ -f ${APKG_CONFIG_BACKUP} ]; then
+  echo "Restore ${APKG_MODULE} config"
+  cp ${APKG_CONFIG_BACKUP} ${APKG_CONFIG}
+  cp -r ${DATA_DIR_BACKUP} ${DATA_DIR}
+fi
+
+echo "Addon ${APKG_MODULE} (install.sh) done" >> $log
 
