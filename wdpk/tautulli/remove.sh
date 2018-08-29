df7bf5a81e6d053da74457c13d0494bf601ef562
Stefaan Ghysels <stefaang@gmail.com>
2018-08-29 18:42:18 +0200

    Cleaner backups

--- wdpk/tautulli/remove.sh
+++ wdpk/tautulli/remove.sh
@@ -7,10 +7,15 @@ APKG_PATH=$1
 # keep config
 APKG_MODULE="tautulli"
 APKG_CONFIG="${APKG_PATH}/config.ini"
-APKG_CONFIG_BACKUP="/mnt/HD/HD_a2/.systemfile/{APKG_MODULE}.ini"
+APKG_CONFIG_BACKUP="/mnt/HD/HD_a2/.systemfile/${APKG_MODULE}.ini"
 DATA_DIR="${APKG_PATH}/data"
-DATA_DIR_BACKUP="/mnt/HD/HD_a2/.systemfile/{APKG_MODULE}_data"
+DATA_DIR_BACKUP="/mnt/HD/HD_a2/.systemfile/${APKG_MODULE}_data"
 
+# remove old backups
+rm ${APKG_CONFIG_BACKUP}
+rm -rf ${DATA_DIR_BACKUP}
+
+# store new backup
 mv ${APKG_CONFIG} ${APKG_CONFIG_BACKUP}
 mv ${DATA_DIR} ${DATA_DIR_BACKUP}
 
