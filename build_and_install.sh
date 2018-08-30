3875645413702fa58bb46a268fe237bcabdf82e3
Stefaan Ghysels <stefaang@gmail.com>
2018-08-31 00:44:32 +0200

    Added instructions

--- build_and_install.sh
+++ build_and_install.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
+#
+# Builds packages and installs it on a target platform
+#
+# Usage:
+#
+#  ./build_and_install package target
+#
+# with 
+#    package: a wdpk e.g. entware
+#    target: the host to install it on
+# 
+# It's recommended to setup entware + ssh keys first
+#    ssh-copy-id root@host
+#
+# Author: TFL
 
 PACKAGE=$1
-
+# TODO: add docstring / help
+# TODO: check if this docker image exists
 
 # create packages
 docker run -it -v $(pwd):/wdpksrc wdpk /bin/bash -c "cd wdpk/$PACKAGE; ./build.sh; chown -R 1000:1000 ../../packages/$PACKAGE"
@@ -16,8 +32,12 @@ TARGET=$2
 
 [[ -z $TARGET ]] && exit 0
 
+# upload the app
 scp $BINARY root@$TARGET:/mnt/HD/HD_a2/.systemfile/upload/app.bin
+
 cssh='ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null'
+
+# install the app
 $cssh root@$TARGET "/usr/local/modules/usrsbin/upload_apkg -rapp.bin -d -f1 -g1"
 
-
+# TODO add test hooks
