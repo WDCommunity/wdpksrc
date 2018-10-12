4ae98a68d119cc289f7ab368bd3d1cd7b36c6570
Stefaan Ghysels <stefaang@gmail.com>
2018-10-12 15:06:26 +0200

    Improved firmware linking

--- wdpk/tvmosaic/start.sh
+++ wdpk/tvmosaic/start.sh
@@ -3,7 +3,11 @@
 . $(dirname $0)/incl
 
 #copy firmware to /lib/firmware
-cp -rf ${TVM_ROOT_DIR}/share/firmware/* /lib/firmware/
+if [ -d /lib/firmware ]; then
+	ln -s ${TVM_ROOT_DIR}/share/firmware/* /lib/firmware/
+else
+	ln -s ${TVM_ROOT_DIR}/share/firmware /lib/firmware
+fi
 
 export LD_LIBRARY_PATH=${TVM_ROOT_DIR}/lib
 export TVMOSAIC_ROOT_CONFIG_DIR=${TVM_ROOT_DIR}
