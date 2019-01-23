0416d69458387787fdade3b925b55d0494e59d52
Stefaan Ghysels <stefaang@gmail.com>
2019-01-23 09:31:19 +0100

    Invalidate old entware root directories

--- wdpk/entware/preinst.sh
+++ wdpk/entware/preinst.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
-[ -f /tmp/debug_apkg ] && echo "APKG_DEBUG: $0 $@" >> /tmp/debug_apkg
+[[ -f /tmp/debug_apkg ]] && echo "APKG_DEBUG: $0 $@" >> /tmp/debug_apkg
 
 APKG_PATH=$1
 
+# invalidate old entware installation leftovers
+# restore them if necessary
+ENTWARE_ROOT=/shares/Volume_1/entware
+BACKUP=${ENTWARE_ROOT}.bak
+[[ -d ${ENTWARE_ROOT} ]] && mv ${ENTWARE_ROOT} ${BACKUP}
