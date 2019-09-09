689b863c16b49c0b0e774a8a18da5e4cc78b8d61
Stefaan Ghysels <stefaang@gmail.com>
2019-09-09 09:18:18 +0200

    Cleanup

--- wdpk/phpMyAdmin/before_apkg.sh
+++ wdpk/phpMyAdmin/before_apkg.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
 
 # DO NOT REMOVE!!
-# This is for APKG Joomla backward compatibility(version 1.00).
-# backup config files SHOULD BE wrote into preinst.sh, and this JUST for APKG Joomla backward compatibility.
