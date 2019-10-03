89634a3aca3a8182a7bef607ee2c4fbfc4530a02
Stefaan Ghysels <stefaang@gmail.com>
2019-10-03 13:59:01 +0200

    Bump and harden syncthing

--- wdpk/syncthing/remove.sh
+++ wdpk/syncthing/remove.sh
@@ -2,12 +2,13 @@
 
 [ -f /tmp/debug_apkg ] && echo "APKG_DEBUG: $0 $@" >> /tmp/debug_apkg
 
+[ -n "$(pidof syncthing)" ] && pkill syncthing
+
 APKG_PATH=$(readlink -f $1)
 . ${APKG_PATH}/env
 
+# remove app
 rm -rf ${APKG_PATH}
-
-# remove lib
 
 # remove web
 rm -rf /var/www/${APKG_NAME}
--- wdpk/syncthing/start.sh
+++ wdpk/syncthing/start.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+log=/tmp/debug_apkg
+
 APKG_PATH=$1
 . ${APKG_PATH}/env
 
@@ -8,9 +10,6 @@ ADDRESS=$(grep -m1 'Require ip' /usr/local/apache2/conf/httpd.conf | awk '{print
 # disable creation of default folder
 export STNODEFAULTFOLDER=0
 
-# keep all persistent data in the syncthing home dir
-ST_HOME=${APKG_PATH}/var
-
 # start the binary
-${APKG_PATH}/syncthing/syncthing -no-browser -home=${ST_HOME} -gui-address="http://${ADDRESS}:${PORT}"
+${APKG_PATH}/syncthing/syncthing -no-browser -home=${ST_HOME} -gui-address="http://${ADDRESS}:${PORT}" 2>&1 >> $log
 
