f7bb8403c7e76f02a353f395de2117d2620999b3
JediNite <gersekow@hotmail.com>
2020-02-20 09:00:20 +1000

    Corrected link location on update_portainer.sh

--- wdpk/docker/update_portainer.sh
+++ wdpk/docker/update_portainer.sh
@@ -7,7 +7,7 @@ docker rm portainer
 docker pull portainer/portainer
 
 # Instantiate the new container
-docker run -d -p 9000:9000 --restart always --name portainer -v /var/run/docker.sock:/var/run/docker.sock -v /mnt/HD/HD_a2/Nas_Prog/docker/portainer:/data portainer/portainer
+docker run -d -p 9000:9000 --restart always --name portainer -v /var/run/docker.sock:/var/run/docker.sock -v $(readlink -f ${APKG_PATH})/portainer:/data portainer/portainer
 
 # Display listing of running containers
 docker ps -a
