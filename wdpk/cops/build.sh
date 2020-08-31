62c7dbf63fbfccadb798ab06584bd244efa8e350
Stefaan Ghysels <stefaang@gmail.com>
2020-09-01 00:47:58 +0200

    Fix cops

--- wdpk/cops/build.sh
+++ wdpk/cops/build.sh
@@ -9,9 +9,8 @@ echo "Building ${APP_NAME} version ${VERSION}"
 
 MODELS="WDMyCloudEX4 WDMyCloudEX2 WDMyCloudMirror WDMyCloud WDMyCloudEX4100 WDMyCloudDL4100 WDMyCloudEX2100 WDMyCloudDL2100 WDMyCloudMirrorGen2 MyCloudEX2Ultra MyCloudPR4100 MyCloudPR2100"
 
-MODELS="MyCloudPR4100 MyCloudPR2100"
 for model in $MODELS; do
-  ../../mksapkg-64b -E -s -m $model > /dev/null
+  ../../mksapkg-OS5 -E -s -m $model > /dev/null
 done
 
 echo "Move binaries"
