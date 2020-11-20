6122d10f84df3076a212e5c7da3d4a2fc4bceb36
JediNite <gersekow@hotmail.com>
2020-11-20 20:50:45 +1000

    OS5 docker support
    - Update "portainer" to use "portainer-ce"
    - Update docker apkg version to 19.03.13
    - Update docker for arm to a maximum version of docker-19.03.8 due to issues with "dockerd" binary on higher versions
    - Simplify logic for building the URL, since OS5 on arm now supports the native docker binaries

--- wdpk/docker/update_portainer.sh
+++ wdpk/docker/update_portainer.sh
@@ -1,17 +1,17 @@
 !#/bin/sh
 
 # Define APKG_PATH
-APKG_PATH="/mnt/HD/HD_a2/Nas_Prog/docker"
+APKG_PATH="/shares/Volume_1/Nas_Prog"
 
 # Stop and remove the old container
-docker stop portainer
-docker rm portainer
+docker stop portainer-ce
+docker rm portainer-ce
 
 # Download the latest container build from dockerhub
-docker pull portainer/portainer
+docker pull portainer/portainer-ce
 
 # Instantiate the new container
-docker run -d -p 9000:9000 --restart always --name portainer -v /var/run/docker.sock:/var/run/docker.sock -v $(readlink -f ${APKG_PATH})/portainer:/data portainer/portainer
+docker run -d -p 9000:9000 --restart always --name portainer-ce -v /var/run/docker.sock:/var/run/docker.sock -v $(readlink -f ${APKG_PATH})/portainer:/data portainer/portainer-ce
 
 # Display listing of running containers
 docker ps -a
