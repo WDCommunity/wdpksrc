9dc5ced16862d83a2c33ef6b5fbb7e6604d73e3c
gautamkrishnar <r.gautamkrishna@gmail.com>
2022-06-27 13:33:58 +0530

    fixed syncthing running as root error and permission issues on share

--- wdpk/syncthing/start.sh
+++ wdpk/syncthing/start.sh
@@ -8,9 +8,8 @@ APKG_PATH=$1
 # get first match for IP address
 ADDRESS=$(sed -n '/ip/ {s/.*<ip>\(\S*\)<\/ip>/\1/p;q}' /etc/NAS_CFG/config.xml )
 
-# disable creation of default folder
-export STNODEFAULTFOLDER=0
+# finds first user in the admin group
+ADMIN_USER=$(cat /etc/group | grep administrators | head -n 1 | awk -F: '{ print $4}')
 
-# start the binary
-${APKG_PATH}/syncthing/syncthing -no-browser -home=${ST_HOME} -gui-address="http://${ADDRESS}:${PORT}" 2>&1 >> $log
-
+# start the binary as ADMIN_USER
+sudo -H -u $ADMIN_USER ${APKG_PATH}/syncthing/syncthing --no-default-folder -no-browser -home=${ST_HOME} -gui-address="http://${ADDRESS}:${PORT}" 2>&1 >> $log
