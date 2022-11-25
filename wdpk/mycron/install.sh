e8f638fcee46cb933b133c587b98f8a2eb345bcb
Pi <pi@pavel-pi.de>
2022-11-25 13:52:29 +0100

    user needs to be created on each reboot

--- wdpk/mycron/install.sh
+++ wdpk/mycron/install.sh
@@ -16,13 +16,6 @@ log "Script called: $0 $@"
 # move from upload path to install path
 mv $UPLOAD_PATH $INST_PATH
 
-
-# create user mycron
-# -D        Don't assign a password
-# -H		Don't create home directory
-# -G root	Add to group "root"
-adduser -D -H www-data
-
 # make SUID copy of sudo binary
 cp /usr/bin/sudo $INST_PATH/mycron/bin/sudo
 chmod 4755 $INST_PATH/mycron/bin/sudo
--- wdpk/mycron/start.sh
+++ wdpk/mycron/start.sh
@@ -10,5 +10,12 @@ function log {
 # log entry
 log "Script called: $0 $@"
 
+# create user mycron
+# -D        Don't assign a password
+# -H		Don't create home directory
+# -G root	Add to group "root"
+adduser -D -H www-data
+
 # set crontab
 crontab -u www-data "$1"/bin/cron.entry
+
