4c11255472ee7e0fa16a0e0b2217f1fdc2a34415
Pi <pi@pavel-pi.de>
2024-02-20 17:11:16 +0100

    added example commands

--- wdpk/myshellscript/bin/myscript.sh
+++ wdpk/myshellscript/bin/myscript.sh
@@ -2,3 +2,16 @@
 . $(dirname $0)/common.sh
 
 log "inside myscript.sh"
+
+# EXAMPLE: run own SSH deamon, e.g. with custom "AuthorizedKeysFile" option
+#pkill sshd
+#/usr/sbin/sshd -p 2222 -E /var/log/sshd.log -f /usr/local/config/sshd_config
+
+# EXAMPLE: access MyCloud via port 443
+#sed -i 's/httpsPort = 8543/httpsPort = 443/g' /etc/nasAdmin.toml
+
+# EXAMPLE: use own certificate (see https://blog.pavel-pi.de/blog/mycloud-custom-cert/)
+#cp /mnt/HD/HD_a2/restsdk-info/data/crypto2/prod/mycert /mnt/HD/HD_a2/restsdk-info/data/crypto2/prod/device-local-<GUID>.remotewd.com 2>/dev/null
+
+# EXAMPLE: restart web service to let above changes (port, certificate) take effect 
+#/usr/sbin/nasAdmin.sh restart
